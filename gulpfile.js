0d9632d18f4eeab6993802cfea38c88a51375d70
chagales <chagales@gmail.com>
2017-08-30 21:25:09 +0200

    Added all common components

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
-var gulp = require("gulp");//importamos gulp
+var gulp = require("gulp"); // importamos la librería gulp
 var sass = require("gulp-sass");
-var notify  = require("gulp-notify");
+var notify = require("gulp-notify");
 var browserSync = require("browser-sync").create();
 var gulpImport = require("gulp-html-import");
+var tap = require("gulp-tap");
+var browserify = require("browserify");
+var buffer = require("gulp-buffer");
+var sourcemaps = require("gulp-sourcemaps");
+var htmlmin = require("gulp-htmlmin");
+var uglify = require("gulp-uglify");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var cssnano = require("cssnano");
+var imagemin = require("gulp-imagemin");
+
 
 
 
 //definimos una tarea por defecto
-gulp.task("default",["html","sass"],function(){
+gulp.task("default",["html","sass","js"],function(){
     console.log("Iniciamos");
     //Iniciamos el servidor de desarrollo
     browserSync.init({server:"dist/"});
@@ -16,6 +27,9 @@ gulp.task("default",["html","sass"],function(){
 
     //observa cambios en los archivos html
     gulp.watch(["src/*.html","src/**/*.html"],["html"]);
+
+    // observa cambios en los archivos JS y entonces ejecuta la tarea 'js'
+    gulp.watch(["src/js/*.js", "src/js/**/*.js"], ["js"]);
 });
 
 //compilar sass
@@ -36,4 +50,25 @@ gulp.task("html", function(){
         .pipe(gulp.dest("dist/"))
         .pipe(browserSync.stream())
         .pipe(notify("HTML importado"));
+});
+
+// compilar y generar un único javascript
+gulp.task("js", function(){
+    gulp.src("src/js/main.js")
+        .pipe(tap(function(file){ // tap nos permite ejecutar una función por cada fichero seleccionado en gulp.src
+            // reemplazamos el contenido del fichero por lo que nos devuelve browserify pasándole el fichero
+            file.contents = browserify(file.path, {debug: true}) // creamos una instancia de browserify en base al archivo
+                            .transform("babelify", {presets: ["es2015"]}) // traduce nuestro codigo de ES6 -> ES5
+                            .bundle() // compilamos el archivo
+                            .on("error", function(error){ // en caso de error, mostramos una notificación
+                                return notify().write(error);
+                            });
+        }))
+        .pipe(buffer()) // convertimos a buffer para que funcione el siguiente pipe
+        .pipe(sourcemaps.init({loadMaps: true})) // captura los sourcemaps del archivo fuente
+        .pipe(uglify()) // minificamos el JavaScript
+        .pipe(sourcemaps.write('./')) // guarda los sourcemaps en el mismo directorio que el archivo fuente
+        .pipe(gulp.dest("dist/")) // lo guardamos en la carpeta dist
+        .pipe(browserSync.stream()) // recargamos el navegador
+        .pipe(notify("JS Compilado"));
 });
