351e69cadd2cc9563dd3aa73ea47d9e38902c0ce
chagales <chagales@gmail.com>
2017-09-03 22:41:00 +0200

    Add like function and goto detalle in Icon Comment

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ window.$ = window.jQuery = require("jquery");
 import CommentsService from "./CommentsService";
 import CommentsListManager from "./CommentsListManager";
 import CommentFormManager from "./CommentFormManager";
+import LikeManager from "./LikeManager.js"
 import PubSub from "pubsub-js";
 
 const commentService = new CommentsService("/comments/");
@@ -13,3 +14,8 @@ commentsListManager.init();
 
 const commentFormManager = new CommentFormManager(".comment-form", commentService, PubSub);
 commentFormManager.init();
+
+const likeManager = new LikeManager();
+likeManager.init();
+likeManager.findlike();
+
